9ea4ee291960c646093b0c7ab59bb74db3ec3eeb
rooneyrama77 <ramakrishnan07041999@gmail.com>
2018-03-04 22:14:04 +0530

    initial module without exceptions

---
+++ scrap.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+import json
+
+print("enter the query as single word eg:rogerfederer and not roger federer")
+
+print("Please mind your spaces")
+
+name=input()
+
+link = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+name+"&limit=10&format=json"
+
+f = urllib.request.urlopen(link)
+
+myfile = f.read()
+
+x=json.loads(myfile)
+
+print(', '.join(x[2]))
